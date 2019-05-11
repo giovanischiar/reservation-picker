4d50cb3d56c721471a4583d1c51d9d78eda88868
Giovani Schiar <giovanischiar@gmail.com>
2019-05-11 04:13:00 -0300

    All Utilities classes documented

--- app/src/main/java/io/schiar/tcc/utilities/BitmapLoader.kt
+++ app/src/main/java/io/schiar/tcc/utilities/BitmapLoader.kt
@@ -4,9 +4,17 @@ import android.widget.ImageView
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.RequestCreator
 
+/**
+ * Carrega imagens.
+ * @param urlString URL de uma imagem.
+ * @property requestCreator carregador de imagens em URL.
+ */
 class BitmapLoader(urlString: String) {
     private val requestCreator: RequestCreator = Picasso.get().load(urlString)
 
+    /**
+     * Carrega em uma [ImageView] uma imagem trazida da WEB
+     */
     fun loadInto(imageView: ImageView) {
         requestCreator.into(imageView)
     }
--- app/src/main/java/io/schiar/tcc/utilities/DateFormatter.kt
+++ app/src/main/java/io/schiar/tcc/utilities/DateFormatter.kt
@@ -3,7 +3,15 @@ package io.schiar.tcc.utilities
 import java.text.DateFormat
 import java.util.*
 
+/**
+ * Formata datas.
+ */
 class DateFormatter {
+    /**
+     * Formata uma data em milisegundos desde o unixtime para uma representação em string seguindo o formato brasileiro.
+     * @param millisecondsSinceEpoch data em milisegundos desde o unixtime.
+     * @return a data formatada em string seguindo o formato brasileiro (DD/MM/AAAA).
+     */
     fun dateString(millisecondsSinceEpoch: Long): String {
         val date = Date(millisecondsSinceEpoch)
             val formatter = DateFormat.getDateInstance(DateFormat.SHORT, Locale("pt", "BR"))
